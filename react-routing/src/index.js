768fa72d882d4fc8e4d38bb0318ec4946e824a1e
ram <kamparapusriram@gmail.com>
2025-10-17 22:39:25 +0530

    done for today

--- react-routing/src/index.js
+++ react-routing/src/index.js
@@ -1,13 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter , Routes , Route , useParams} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+const About = () => {
+  return (
+    <div>
+      <h1> About Page </h1>
+    </div>
+  )
+}
+
+const Profile = () => {
+  return (
+    <div>
+      <h1> Profile Page</h1>
+    </div>
+  )
+}
+const Settings = () => {
+  return (
+    <div>
+      <h1> Settings Page</h1>
+    </div>
+  )
+}
+
+const SayUser = () => {
+  const params = useParams();
+  console.log("params" , params);
+
+  return (
+    <div>
+      <h1> Your Name is {params.userName}</h1>
+    </div>
+  )
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+        <Route path = "/about" element = {<About />} />
+        <Route path = "/account/profile" element = {<Profile />} />
+
+        <Route path="/user/:userName" element = {<SayUser />} />
+
+        <Route path = "/account">
+          <Route path = "settings" element = {<Settings />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
