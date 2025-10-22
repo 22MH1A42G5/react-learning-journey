e7ce82c57206855ac947593a01792530e0b5baac
ram <kamparapusriram@gmail.com>
2025-10-22 10:36:04 +0530

    working with firestore

--- firebase-in-react/src/App.jsx
+++ firebase-in-react/src/App.jsx
@@ -1,13 +1,38 @@
-
-import React from "react"
+import { signOut } from "firebase/auth";
+import { useFirebase } from "./context/Firebase";
+import React, { useState } from "react"
 
 function App() {
-
-  return (
-    <div>
-      <h1>Firebase</h1>
-    </div>
-  )
+  const firebase = useFirebase();
+  const [email , setEmail] = useState('');
+  const [password , setPassword] = useState('');
+  console.log(firebase);
+  if(firebase.user === null){
+    return (
+      <div>
+        <h1>Firebase</h1>
+        <input onChange={e => setEmail(e.target.value)} value = {email} type="email" placeholder="Enter Email" />
+        <input onChange={e => setPassword(e.target.value)} value = {password} type="password" placeholder="Enter Password" />
+        <button
+          onClick = { () => {
+            firebase.signupUserWithEmailAndPassword(email , password)
+            firebase.putData('user/' + "sriram" , {email , password});
+          }}>  Sign Up
+        </button>
+        <h1>Signin With Google</h1>
+        <button onClick={e => firebase.signUpWithGoogle()}>With Google</button>
+      </div>
+    )
+  }
+  else{
+    // console.log(firebase.user.displayName);
+    return (
+      <>
+        <h1>Hello , {firebase.user.displayName} </h1>
+        <button onClick={ () => firebase.Logout()}>log out</button>
+      </>
+    )
+  }
 }
 
 export default App;
--- firebase-in-react/src/context/Firebase.jsx
+++ firebase-in-react/src/context/Firebase.jsx
@@ -1,12 +1,63 @@
-import { createContext } from "react";
+import { createContext , useContext, useEffect, useState} from "react";
 import {initializeApp} from 'firebase/app'
+import { getAuth ,createUserWithEmailAndPassword , GoogleAuthProvider, signInWithPopup , onAuthStateChanged, signOut } from 'firebase/auth'
+import { getDatabase ,set , ref } from 'firebase/database'
 
+const GoogleProvider = new GoogleAuthProvider();
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAahHZBMoPR_uggh9txFcVcoAq5ZPQqhyY",
+  authDomain: "app-b9260.firebaseapp.com",
+  databaseURL: "https://app-b9260-default-rtdb.firebaseio.com",
+  projectId: "app-b9260",
+  storageBucket: "app-b9260.firebasestorage.app",
+  messagingSenderId: "915585003629",
+  appId: "1:915585003629:web:d2f54b26764d6c995c4f64",
+};
+
+export const useFirebase = () => useContext(FirebaseContext)
+
+const firebaseApp = initializeApp(firebaseConfig);
+const firebaseAuth = getAuth(firebaseApp);
+const database = getDatabase(firebaseApp);
 
 const FirebaseContext = createContext(null);
 
 export const FirebaseProvider = (props) => {
-    <FirebaseContext.Provider>
-        {props.children}
-    </FirebaseContext.Provider>
+    const [user , setUser] = useState(null);
+
+    const signupUserWithEmailAndPassword = (email , password) => {
+        return createUserWithEmailAndPassword(firebaseAuth , email , password)
+    }
+    const putData = (key , data) => {
+        set(ref(database , key) , data);
+    }
+    useEffect( () => {
+        onAuthStateChanged(firebaseAuth , (user) => {
+            if(user)
+                setUser(user);
+            else{
+                console.log("You are Logged out")
+                setUser(null);
+            }
+        })
+    } , [])
+    const signUpWithGoogle = () => {
+        signInWithPopup(firebaseAuth , GoogleProvider)
+    }
+    const Logout = () => {
+        signOut(firebaseAuth);
+    }
+    return (
+        <FirebaseContext.Provider value={{
+            signupUserWithEmailAndPassword,
+            putData,
+            signUpWithGoogle,
+            user,
+            setUser,
+            Logout,
+        }}>
+            {props.children}
+        </FirebaseContext.Provider>
+    )
 }
